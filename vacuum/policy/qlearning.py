0b4659ed27b7d8675ccc733ee1c5e25a03b2b39a
Hakim Mitiche <hmitens@gmail.com>
2025-04-23 11:55:52 +0100

    small corrections

--- vacuum/policy/qlearning.py
+++ vacuum/policy/qlearning.py
@@ -17,8 +17,10 @@ class QLearnPolicy(CleanPolicy):
 		super().__init__("q-learning", world_id, env)
 		#self._location=Map.locations_list(world_id)
 		#assert self._locations is not None
+		#print("map_size ", self.env.map_size)
 
 	def reset(self):
+		#print("map_size", self.env.map_size)
 		pass
 
 
@@ -32,17 +34,15 @@ class QLearnPolicy(CleanPolicy):
 			return np.argmax(self.q_table[state])
 
 	# Q-Learning Training with Epsilon-Greedy and Linear Epsilon Decay
-	def train_q_learning(self,env, episodes, learning_rate_a=0.1, discount_factor_g=0.95, epsilon=1.0, epsilon_min=0.01,
+	# @PROF: add the code that calls it in  main.py
+	def train_q_learning(self, env, episodes, learning_rate_a=0.1, discount_factor_g=0.95, epsilon=1.0, epsilon_min=0.01,
 						 epsilon_decay_rate=0.001):
 		self.q_table = np.zeros((env.map_size, env.map_size,
 								 2, env.action_space.n))
+		
 		self._episode_reward = 0
-
-
-
 		for episode in range(episodes):
 			state = env.reset()[0]
-
 			while(not terminated and not truncated):
 				action = self.select_action(state)
 				(next_obs, reward, terminated, truncated,
@@ -51,7 +51,6 @@ class QLearnPolicy(CleanPolicy):
 				self.q_table[state, action] = self.q_table[state, action] + learning_rate_a * (
 						reward + discount_factor_g * np.max(self.q_table[new_state, :]) - self.q_table[state, action]
 				)
-
 				self._episode_reward += reward
 				state = new_state
 			epsilon = max(epsilon_min, epsilon - epsilon_decay_rate)
